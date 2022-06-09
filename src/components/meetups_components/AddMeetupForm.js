642c17ea6289af4c75e2861a1fddf3e650cc73e7
hamza <hamzash863@gmail.com>
2022-06-09 18:57:51 +0300

    Add Card component

---
+++ src/components/meetups_components/AddMeetupForm.js
@@ -0,0 +1,4 @@
+function AddNewMeetupForm(props) {
+  return <div></div>;
+}
+export default AddNewMeetupForm;
--- src/components/meetups_components/MeetupItem.js
+++ src/components/meetups_components/MeetupItem.js
@@ -1,20 +1,23 @@
 import classes from "./MeetupItem.module.css";
+import Card from "../Card";
 
 function MeetupItem(props) {
   return (
-    <li className={classes.item}>
-      <div className={classes.image}>
-        <img className="rounded-lg" src={props.image}></img>
-      </div>
-      <div className={classes.content}>
-        <h3 className="text-xl font-bold">{props.title}</h3>
-        <address className="py-1 text-green-700">{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={classes.action}>
-        <button>Add Favorite</button>
-      </div>
-    </li>
+    <Card>
+      <li>
+        <div className={classes.image}>
+          <img className="rounded-lg" src={props.image} alt="Loading..."></img>
+        </div>
+        <div className={classes.content}>
+          <h3 className="text-xl font-bold">{props.title}</h3>
+          <address className="py-1 text-green-700">{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.action}>
+          <button>Add Favorite</button>
+        </div>
+      </li>
+    </Card>
   );
 }
 export default MeetupItem;
