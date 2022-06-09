71dfc48ec0aca1ea9f3cd5a7c02def15c66a07df
hamza <hamzash863@gmail.com>
2022-06-09 15:47:32 +0300

    Header style

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import MeetupsView from "./views/MeetupsView";
 import FavoritesMeetups from "./views/FavoritesMeetups";
 import AddMeetup from "./views/AddMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<MeetupsView />} />
-        <Route path="/add-meetup" element={<AddMeetup />} />
-        <Route path="/favorites" element={<FavoritesMeetups />} />
-      </Routes>
+      <MainNavigation />
+      <div>
+        <Routes>
+          <Route path="/" element={<MeetupsView />} />
+          <Route path="/add-meetup" element={<AddMeetup />} />
+          <Route path="/favorites" element={<FavoritesMeetups />} />
+        </Routes>
+      </div>
     </div>
   );
 }
