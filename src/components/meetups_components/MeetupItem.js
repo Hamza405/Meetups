51ff5356f16f3fceb077ea6d806545c3e9e02909
hamza <hamzash863@gmail.com>
2022-06-10 12:21:26 +0300

    handle form data

--- src/components/meetups_components/MeetupItem.js
+++ src/components/meetups_components/MeetupItem.js
@@ -1,5 +1,5 @@
 import classes from "./MeetupItem.module.css";
-import Card from "../Card";
+import Card from "../ui_element/Card";
 
 function MeetupItem(props) {
   return (
--- src/views/AddMeetup.js
+++ src/views/AddMeetup.js
@@ -1,5 +1,13 @@
+import AddMeetupForm from "../components/meetups_components/AddMeetupForm";
 function AddMeetupView() {
-  return <div></div>;
+  function addMeetup(submitData) {
+    // fetch("https://tishreen-62882-default-rtdb.firebaseio.com/meetups.json");
+  }
+  return (
+    <div>
+      <AddMeetupForm addMeetup={addMeetup} />
+    </div>
+  );
 }
 
 export default AddMeetupView;
