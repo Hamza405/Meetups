f16bd4ea9aab0fcd91330de7325a28383b944630
hamza <hamzash863@gmail.com>
2022-06-10 12:52:55 +0300

    Navigate after submit form

--- src/views/AddMeetup.js
+++ src/views/AddMeetup.js
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import AddMeetupForm from "../components/meetups_components/AddMeetupForm";
 function AddMeetupView() {
+  const navigate = useNavigate();
   function addMeetup(submitData) {
     // TODO: listen to result and show alert with it
     fetch("https://tishreen-62882-default-rtdb.firebaseio.com/meetups.json", {
@@ -8,6 +10,8 @@ function AddMeetupView() {
       headers: {
         "Content-Type": "application/json",
       },
+    }).then(() => {
+      navigate("/", { replace: true });
     });
   }
   return (
