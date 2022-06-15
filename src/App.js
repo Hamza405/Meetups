b6d3124ac9225dbd03eaada20dc66a9acadfc0fd
hamza <hamzash863@gmail.com>
2022-06-15 17:41:10 +0300

    fill favorite view with data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import MeetupsView from "./pages/MeetupsView";
-import FavoritesMeetups from "./pages/FavoritesMeetups";
+import FavoritesMeetupsView from "./pages/FavoritesMeetups";
 import AddMeetup from "./pages/AddMeetup";
 import MainLayout from "./components/layout/MainLayout";
 
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MeetupsView />} />
         <Route path="/add-meetup" element={<AddMeetup />} />
-        <Route path="/favorites" element={<FavoritesMeetups />} />
+        <Route path="/favorites" element={<FavoritesMeetupsView />} />
       </Routes>
     </MainLayout>
   );
